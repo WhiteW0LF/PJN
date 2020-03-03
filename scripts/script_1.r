c3e9b6ab0e7ce0c8fa9712702692a4c8b4a92006
s-117-06 <s-117-06@gumiforest.ae.krakow.pl>
2020-03-03 12:50:07 +0100

    2020-03-03" git push -u origin master git commit -m 2020-03-03 git commit -m '2020-03-03

---
+++ scripts/script_1.r
@@ -0,0 +1,46 @@
+#add libraries
+library('tm')
+
+#project source
+workDir <- 'D:\\PJN'
+setwd(workDir)
+
+#define project folders
+inputDir <- '.\\data'
+outputDir  <- '.\\results'
+scriptsDir <- '.\\scripts'
+workspaceDir <- '.\\workspace'
+
+#create output folder
+dir.create(outputDir, showWarnings = FALSE)
+dir.create(workspaceDir, showWarnings = FALSE)
+
+
+#create corpuse
+corpusDir <- paste(inputDir, '\\', 'Literatura - streszczenia - oryginał', sep = '')
+corpus <- VCorpus(
+ DirSource(
+   corpusDir,
+   pattern = '*.txt',
+   encoding = 'UTF-8'
+ ),
+ readerControl = list(
+   language='pl_PL'
+ )
+)
+
+#initial transform
+corpus <- tm_map(corpus, removeNumbers)
+corpus <- tm_map(corpus, removePunctuation)
+corpus <- tm_map(corpus, content_transformer(tolower))
+
+stoplistFile <- paste(inputDir, '\\', 'stopwords_pl.txt')
+stopList <- readLines(
+  stoplistFile,
+  encoding = 'UTF-8'
+)
+
+corpus <- tm_map(corpus, removeWords, stopList)
+corpus <- tm_map(corpus, stripWhitespace)
+
+writeLines(as.character(corpus[[1]]))
