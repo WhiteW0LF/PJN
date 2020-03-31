05c8143cbdf5d872588345d94fef422c415a65a5
unknown <syniak.int@gmail.com>
2020-03-31 14:30:56 +0200

    SO 31.03.2020

--- scripts/script_1.r
+++ scripts/script_1.r
@@ -45,49 +45,49 @@ stopList <- readLines(
 corpus <- tm_map(corpus, removeWords, stopList)
 corpus <- tm_map(corpus, stripWhitespace)
 
-remove_char <- content_transformer(
+removeChar <- content_transformer(
    function(x, pattern, replacement) 
       gsub(pattern, replacement, x)
 )
 
 #usuniecie "em dash" i 3/4 z tekstow
-corpus <- tm_map(corpus, remove_char, intToUtf8(8722), '')
-corpus <- tm_map(corpus, remove_char, intToUtf8(190), '')
+corpus <- tm_map(corpus, removeChar, intToUtf8(8722), '')
+corpus <- tm_map(corpus, removeChar, intToUtf8(190), '')
 
 #lematyzacja - sprawozdanie do formy podstatowej
 polish <- dictionary(lang = 'pl_PL')
 
 lemmatize <- function(text) {
-   simple_text <- str_trim(as.character(text[1]))
-   parsed_text <- strsplit(simple_text, split = ' ')
-   new_text_vec <- hunspell_stem(parsed_text[[1]], dict = polish)
-   for (i in 1:length(new_text_vec)) {
-      if (length(new_text_vec[[i]]) == 0) new_text_vec[i] <- parsed_text[[1]][i]
-      if (length(new_text_vec[[i]]) > 1) new_text_vec[i] <- new_text_vec[[i]][1]
+   simpleText <- str_trim(as.character(text[1]))
+   parsedText <- strsplit(simpleText, split = ' ')
+   newTextVec <- hunspell_stem(parsedText[[1]], dict = polish)
+   for (i in 1:length(newTextVec)) {
+      if (length(newTextVec[[i]]) == 0) newTextVec[i] <- parsedText[[1]][i]
+      if (length(newTextVec[[i]]) > 1) newTextVec[i] <- newTextVec[[i]][1]
    }
-   new_text <- paste(new_text_vec, collapse = ' ')
-   return(new_text)
+   newText <- paste(newTextVec, collapse = ' ')
+   return(newText)
 }
 
 corpus <- tm_map(corpus, content_transformer(lemmatize))
 
 #usuniecie rozszerzen z nazw dokumentoW
-cut_extention <- function(document) {
+cutExtention <- function(document) {
    meta(document, 'id') <- gsub(pattern = '\\.txt$', '', meta(document, 'id'))
    return(document)
 }
 
-corpus <- tm_map(corpus, cut_extention)
+corpus <- tm_map(corpus, cutExtention)
 
 #export korpusu przetwarzonego do plikow textowych
-preprocessed_dir <- paste(
+preprocessedDir <- paste(
    outputDir,
    '\\',
    'Literatura - streszczenia - przetworzone',
    sep = ''
 )
-dir.create(preprocessed_dir, showWarnings = FALSE)
-writeCorpus(corpus, path = preprocessed_dir)
+dir.create(preprocessedDir, showWarnings = FALSE)
+writeCorpus(corpus, path = preprocessedDir)
 
 
 
---
+++ scripts/script_2.r
@@ -0,0 +1,114 @@
+#add libraries
+library('tm')
+
+#project source
+workDir <- 'D:\\uek\\PJN'
+setwd(workDir)
+
+#define project folders
+inputDir <- '.\\data'
+outputDir  <- '.\\results'
+scriptsDir <- '.\\scripts'
+workspaceDir <- '.\\workspace'
+
+#create output folder
+dir.create(outputDir, showWarnings = FALSE)
+dir.create(workspaceDir, showWarnings = FALSE)
+
+
+#create corpuse
+corpusDir <- paste(
+  inputDir,
+  '\\',
+  'Literatura - streszczenia - preprocessed',
+  sep = ''
+)
+
+corpus <- VCorpus(
+  DirSource(
+    corpusDir,
+    pattern = '*.txt',
+    encoding = 'UTF-8'
+  ),
+  readerControl = list(
+    language='pl_PL'
+  )
+)
+
+#usuniecie rozszerzen z nazw dokumentoW
+cutExtention <- function(document) {
+  meta(document, 'id') <- gsub(pattern = '\\.txt$', '', meta(document, 'id'))
+  return(document)
+}
+
+corpus <- tm_map(corpus, cutExtention)
+
+#create macierz czestosci
+tdmTfAll <- TermDocumentMatrix(corpus)
+dtmTfAll <- DocumentTermMatrix(corpus)
+tdmTfidfAll <- TermDocumentMatrix(
+  corpus,
+  control = list(
+    weighting = weightTfIdf
+  )
+)
+tdmBinAll <- TermDocumentMatrix(
+  corpus,
+  control = list(
+    weighting = weightBin
+  )
+)
+tdmTfBounds <- TermDocumentMatrix(
+  corpus,
+  control = list(
+    bounds = list(
+      global = c(2,16)
+    )
+  )
+)
+tdmTfIdfBounds <- TermDocumentMatrix(
+  corpus,
+  control = list(
+    weighting = weightTfIdf,
+    bounds = list(
+      global = c(2,16)
+    )
+  )
+)
+dtmTfIdfBounds <- DocumentTermMatrix(
+  corpus,
+  control = list(
+    weighting = weightTfIdf,
+    bounds = list(
+      global = c(2,16)
+    )
+  )
+)
+
+#konwersja macierzy zadkich do macierzy klasycznych
+tdmTfAllMatrix <- as.matrix(tdmTfAll)
+dtmTfAllMatrix <- as.matrix(dtmTfAll)
+tdmTfidfAllMatrix <- as.matrix(tdmTfidfAll)
+tdmBinAllMatrix <- as.matrix(tdmBinAll)
+tdmTfBoundsMatrix <- as.matrix(tdmTfBounds)
+tdmTfIdfBoundsMatrix <- as.matrix(tdmTfIdfBounds)
+dtmTfIdfBoundsMatrix <- as.matrix(dtmTfIdfBounds)
+
+#eksport macierzy do pliku .csv
+#matrixFile <- paste(
+#  outputDir,
+#  '\\',
+#  'tdmTfIdfBounds.csv',
+#  sep = ''
+#)
+
+write.table(
+  tdmTfIdfBoundsMatrix,
+  file = matrixFile,
+  sep = ';',
+  dec = ',',
+  col.names = NA
+)
+
+
+
